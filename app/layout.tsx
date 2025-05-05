f40b5755037bbf1b2fe0a441edc0f7a13906def8
cbeAbishek <agsabhishek1907@gmail.com>
2025-05-05 16:17:15 +0530

    meta file added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,37 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'PPG IT ',
+  title: 'PPG Institute of Technology',
   description: 'Shaping the future through innovation, knowledge, and leadership',
   icons: {
     icon: '/logo.png',
     shortcut: '/logo.png',
     apple: '/logo.png',
   },
+  openGraph: {
+    title: 'PPG Institute of Technology',
+    description: 'Shaping the future through innovation, knowledge, and leadership',
+    url: 'https://www.ppgit.edu.in',
+    siteName: 'PPGIT',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/cbeAbishek/PPG-fullstack/refs/heads/main/public/ppgmeta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'PPG Institute of Technology - Shaping the Future',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'PPG Institute of Technology',
+    description: 'Shaping the future through innovation, knowledge, and leadership',
+    images: [
+      'https://raw.githubusercontent.com/cbeAbishek/PPG-fullstack/refs/heads/main/public/ppgmeta.jpg',
+    ],
+  },
+  themeColor: '#ff914d',
 }
 
 export default function RootLayout({
